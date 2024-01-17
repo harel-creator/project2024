215620522f65172959376609b04d1555a3fe2e8f
harel_creator <harelto@yahoo.com>
2024-01-17 20:02:26 +0200

    split method to split the input

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -4,9 +4,11 @@
 class BloomFilter{
     private:
         HashFunc* hashF;
+        std::vector<bool> filter;
     public:
         BloomFilter(){
             this->hashF = new OneHashFunc();
+            this->filter = {};
     
         }
         ~BloomFilter(){
@@ -20,19 +22,25 @@ class BloomFilter{
             std::hash<std::string> myStringHash;
             return this->hashF->hash(url);
         }
+
+      
         /*
         split the str by "" to an array. Need to delete the array after use
         */
-        std::string* split(std::string str){
+        std::vector<std::string> split(std::string str){
+            std::vector<std::string> splited_str{};
             std::stringstream stream_str(str);
             std::string single_word = "";
-            std::string* string_array = new std::string[2];
-            int i = 0;
+
             while(stream_str >> single_word){
-                string_array[i] = single_word;
-                i++;
+                splited_str.push_back(single_word);
             }
-            return string_array;
+            
+            /*std::cout<<"we start a new array, its size is: "<<splited_str.size()<<std::endl;
+            for(auto word = splited_str.begin(); word!= splited_str.end(); word++){
+                std::cout<<*word<<std::endl;
+            }*/
+            return splited_str;
         }
         
 
--- tests/bloom_test.cpp
+++ tests/bloom_test.cpp
@@ -16,16 +16,28 @@ TEST(HashFunc, HashBasicTest){
     std::hash<std::string> myStringHash;
     EXPECT_EQ(hf.hash("6"), myStringHash("6")%8);
 }
+
 // Make sure that the split realy split by "".
-TEST(SlpitTest, BsicSplitTest){
+TEST(SlpitTests, BasicSplitTest){
     BloomFilter bf;
     std::string s = "2 www.com11";
-    std::string ans[2] = {"2", "www.com11"}; 
-    //bf.setInfo(bf.split(s));
-    EXPECT_EQ(bf.split(s)->size(), ans->size());
-    for (int i = 0; i <= ans->size(); i++){
-        EXPECT_EQ(bf.split(s)[i], ans[i]);
+    std::vector<std::string> ans = {"2", "www.com11"}; 
+    std::vector<std::string> splited_str;
+    splited_str = bf.split(s);
+    ASSERT_EQ(splited_str.size(),ans.size());
+    for (int i = 0; i < ans.size(); i++){
+        EXPECT_EQ(splited_str[i], ans[i]);
     }
-
 }
 
+TEST(SlpitTests, BasicSplitTest2){
+    BloomFilter bf;
+    std::string s = "2 1 www.com11";  
+    std::vector<std::string> ans = {"2", "1", "www.com11"}; 
+    std::vector<std::string> splited_str;
+    splited_str = bf.split(s);
+    ASSERT_EQ(splited_str.size(),ans.size());
+    for (int i = 0; i < ans.size(); i++){
+        EXPECT_EQ(splited_str[i], ans[i]);
+    }
+}
